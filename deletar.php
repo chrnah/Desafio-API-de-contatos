8493e87653a2fddcb1067e0a482f62ab08aaf43f
Christian <79052398+chrnah@users.noreply.github.com>
2022-03-20 18:49:16 -0300

    Update deletar.php

--- deletar.php
+++ deletar.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "DELETE FROM contatos WHERE nome,=0";
+$sql = "DELETE FROM contatos WHERE id = '4' ";
 
 if ($conn->query($sql) === TRUE) {
     echo "deletado com sucesso";
